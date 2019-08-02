d2dd2987813f7e7c1c12be1dc8bf7c41c29855b3
Dylan Harries <Dylan.Harries@csiro.au>
2019-08-02 17:59:06 +1000

    Add additional tests for kernel AA

--- test/test_kernel_aa.cpp
+++ test/test_kernel_aa.cpp
@@ -6,6 +6,7 @@
 
 #include "comparison_helpers.hpp"
 
+#include <algorithm>
 #include <random>
 
 using namespace reor;
@@ -501,6 +502,180 @@ TEST_CASE("Test weights update with Eigen matrices",
 
       const auto updated_S = aa.get_weights();
       CHECK(is_left_stochastic_matrix(updated_S, 1e-12));
+   }
+}
+
+TEST_CASE("Test solution for kernel AA with Eigen matrices",
+          "[kernel_aa][eigen_backend]")
+{
+   using Backend = backends::Eigen_backend<double>;
+
+   const int seed = 0;
+   std::mt19937 generator(seed);
+
+   SECTION("Finds elements of 3 point convex hull as archetypes for 2D example")
+   {
+      const int n_features = 2;
+      const int n_samples = 50;
+      const int n_components = 3;
+      const int max_iter = 500;
+      const double tolerance = 1e-6;
+
+      Eigen::MatrixXd basis(n_features, n_components);
+      basis << 0, 1, 0,
+         0, 0, 1;
+
+      Eigen::MatrixXd expected_S(n_components, n_samples);
+      random_left_stochastic_matrix(expected_S, generator);
+
+      const std::vector<int> assignments({5, 27, 32});
+      for (int i = 0; i < n_components; ++i) {
+         expected_S.col(assignments[i]) = Eigen::VectorXd::Zero(n_components);
+         expected_S(i, assignments[i]) = 1;
+      }
+
+      const Eigen::MatrixXd X(basis * expected_S);
+      const Eigen::MatrixXd K(X.transpose() * X);
+
+      Eigen::MatrixXd C(n_samples, n_components);
+      Eigen::MatrixXd S(n_components, n_samples);
+
+      random_left_stochastic_matrix(C, generator);
+      random_left_stochastic_matrix(S, generator);
+
+      REQUIRE(is_left_stochastic_matrix(C, 1e-12));
+      REQUIRE(is_left_stochastic_matrix(S, 1e-12));
+
+      const double delta = 0;
+      KernelAA<Backend> aa(K, C, S, delta);
+
+      double cost_delta = std::numeric_limits<double>::max();
+      double old_cost = aa.cost();
+      double new_cost = old_cost;
+      int iter = 0;
+      int error = 0;
+      while (std::abs(cost_delta) > tolerance && iter < max_iter) {
+         old_cost = new_cost;
+
+         error = aa.update_dictionary();
+         REQUIRE(error == 0);
+
+         new_cost = aa.cost();
+         CHECK(new_cost <= old_cost);
+
+         error = aa.update_weights();
+         REQUIRE(error == 0);
+
+         new_cost = aa.cost();
+         CHECK(new_cost <= old_cost);
+
+         cost_delta = new_cost - old_cost;
+
+         CHECK(cost_delta <= 0);
+
+         ++iter;
+      }
+
+      REQUIRE(iter < max_iter);
+
+      const Eigen::MatrixXd solution_C(aa.get_dictionary());
+      const Eigen::MatrixXd solution_S(aa.get_weights());
+
+      CHECK(is_left_stochastic_matrix(solution_C, 1e-12));
+      CHECK(is_left_stochastic_matrix(solution_S, 1e-12));
+
+      std::vector<int> main_components(n_components);
+      for (int i = 0; i < n_components; ++i) {
+         solution_C.col(i).maxCoeff(&main_components[i]);
+      }
+      std::sort(std::begin(main_components), std::end(main_components));
+
+      for (int i = 0; i < n_components; ++i) {
+         CHECK(main_components[i] == assignments[i]);
+      }
+   }
+
+   SECTION("Finds elements of 8 point convex hull as archetypes for 3D example")
+   {
+      const int n_features = 3;
+      const int n_samples = 100;
+      const int n_components = 8;
+      const int max_iter = 500;
+      const double tolerance = 1e-6;
+
+      Eigen::MatrixXd basis(n_features, n_components);
+      basis << 0, 1, 1, 0, 0, 1, 1, 0,
+         0, 0, 1, 1, 0, 0, 1, 1,
+         0, 0, 0, 0, 1, 1, 1, 1;
+
+      Eigen::MatrixXd expected_S(n_components, n_samples);
+      random_left_stochastic_matrix(expected_S, generator);
+
+      const std::vector<int> assignments({8, 9, 15, 20, 34, 56, 78, 90});
+      for (int i = 0; i < n_components; ++i) {
+         expected_S.col(assignments[i]) = Eigen::VectorXd::Zero(n_components);
+         expected_S(i, assignments[i]) = 1;
+      }
+
+      const Eigen::MatrixXd X(basis * expected_S);
+      const Eigen::MatrixXd K(X.transpose() * X);
+
+      Eigen::MatrixXd C(n_samples, n_components);
+      Eigen::MatrixXd S(n_components, n_samples);
+
+      random_left_stochastic_matrix(C, generator);
+      random_left_stochastic_matrix(S, generator);
+
+      REQUIRE(is_left_stochastic_matrix(C, 1e-12));
+      REQUIRE(is_left_stochastic_matrix(S, 1e-12));
+
+      const double delta = 0;
+      KernelAA<Backend> aa(K, C, S, delta);
+
+      double cost_delta = std::numeric_limits<double>::max();
+      double old_cost = aa.cost();
+      double new_cost = old_cost;
+      int iter = 0;
+      int error = 0;
+      while (std::abs(cost_delta) > tolerance && iter < max_iter) {
+         old_cost = new_cost;
+
+         error = aa.update_dictionary();
+         REQUIRE(error == 0);
+
+         new_cost = aa.cost();
+         CHECK(new_cost <= old_cost);
+
+         error = aa.update_weights();
+         REQUIRE(error == 0);
+
+         new_cost = aa.cost();
+         CHECK(new_cost <= old_cost);
+
+         cost_delta = new_cost - old_cost;
+
+         CHECK(cost_delta <= 0);
+
+         ++iter;
+      }
+
+      REQUIRE(iter < max_iter);
+
+      const Eigen::MatrixXd solution_C(aa.get_dictionary());
+      const Eigen::MatrixXd solution_S(aa.get_weights());
+
+      CHECK(is_left_stochastic_matrix(solution_C, 1e-12));
+      CHECK(is_left_stochastic_matrix(solution_S, 1e-12));
+
+      std::vector<int> main_components(n_components);
+      for (int i = 0; i < n_components; ++i) {
+         solution_C.col(i).maxCoeff(&main_components[i]);
+      }
+      std::sort(std::begin(main_components), std::end(main_components));
+
+      for (int i = 0; i < n_components; ++i) {
+         CHECK(main_components[i] == assignments[i]);
+      }
    }
 }
 
