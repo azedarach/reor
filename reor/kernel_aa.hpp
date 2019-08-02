57a863f29995f6ba9af984630369bdcb1e6ac804
Dylan Harries <Dylan.Harries@csiro.au>
2019-08-02 15:18:24 +1000

    Use SPG method for weight and dictionary updates

--- reor/kernel_aa.hpp
+++ reor/kernel_aa.hpp
@@ -8,8 +8,11 @@
 
 #include "backend_interface.hpp"
 #include "numerics_helpers.hpp"
+#include "spg.hpp"
 
+#include <deque>
 #include <stdexcept>
+#include <tuple>
 
 #include <iostream>
 
@@ -59,6 +62,7 @@ private:
    Matrix C_old{};
    Matrix grad_C{};
    Matrix incr_C{};
+   Matrix delta_grad_C{};
 
    Matrix diag_alpha_old{};
    Matrix grad_alpha{};
@@ -67,17 +71,23 @@ private:
    Matrix S_old{};
    Matrix grad_S{};
    Matrix incr_S{};
+   Matrix delta_grad_S{};
 
    double beta{0.5};
    double sigma{0.01};
-   double alpha_C{1};
-   double alpha_S{1};
    double alpha_scale_factors{1};
 
+   SPG_line_search_parameters<Real> line_search_parameters{};
+   double alpha_C{1};
+   double alpha_S{1};
+   std::size_t mem{1};
+   std::deque<double> f_C_mem{};
+   std::deque<double> f_S_mem{};
+
    void update_dictionary_gradient();
-   int dictionary_line_search();
+   std::tuple<int, double> dictionary_line_search();
    void update_weights_gradient();
-   int weights_line_search();
+   std::tuple<int, double> weights_line_search();
    void update_scale_factors_gradient();
    int scale_factors_line_search();
 };
@@ -120,6 +130,7 @@ KernelAA<Backend>::KernelAA(
 
    C_old = Backend::copy_matrix(C_);
    grad_C = Backend::create_matrix(n_samples, n_components);
+   delta_grad_C = Backend::create_matrix(n_components, n_samples);
    incr_C = Backend::create_matrix(n_samples, n_components);
 
    diag_alpha_old = Backend::copy_matrix(diag_alpha);
@@ -128,7 +139,11 @@ KernelAA<Backend>::KernelAA(
 
    S_old = Backend::copy_matrix(S_);
    grad_S = Backend::create_matrix(n_components, n_samples);
+   delta_grad_S = Backend::create_matrix(n_components, n_samples);
    incr_S = Backend::create_matrix(n_components, n_samples);
+
+   f_S_mem = std::deque<double>(mem, 0);
+   f_C_mem = std::deque<double>(mem, 0);
 }
 
 template <class Backend>
@@ -154,10 +169,10 @@ void KernelAA<Backend>::set_line_search_sigma(double s)
 template <class Backend>
 double KernelAA<Backend>::cost()
 {
-   backends::gemm(1, S, S, 1, SSt,
+   backends::gemm(1, S, S, 0, SSt,
                   backends::Op_flag::None, backends::Op_flag::Transpose);
 
-   backends::gemm(1, K, C, 1, KC);
+   backends::gemm(1, K, C, 0, KC);
    if (delta > 0) {
       backends::gemm(1, KC, diag_alpha, 0, KC);
    }
@@ -169,94 +184,113 @@ double KernelAA<Backend>::cost()
    }
 
    return backends::trace_gemm_op(1, CtKC, SSt)
-      + backends::trace_gemm_op(-2, KC, S) + trace_K;
+      - 2 * backends::trace_gemm_op(1, KC, S) + trace_K;
 }
 
 template <class Backend>
 void KernelAA<Backend>::update_weights_gradient()
 {
-   const std::size_t n_samples = backends::cols(S);
-   const double normalization = n_samples / trace_K;
-
    backends::gemm(1, C, K, 0, grad_S,
                   backends::Op_flag::Transpose, backends::Op_flag::Transpose);
    if (delta > 0) {
       backends::gemm(1, diag_alpha, grad_S, 0, grad_S);
    }
-   backends::gemm(normalization, CtKC, S, -normalization, grad_S);
+   backends::gemm(2, CtKC, S, -2, grad_S);
 }
 
 template <class Backend>
-int KernelAA<Backend>::weights_line_search()
+std::tuple<int, double> KernelAA<Backend>::weights_line_search()
 {
-   const double safety = std::numeric_limits<double>::min();
-
-   const double current_cost = cost();
+   const double current_cost = trace_K
+      - 2 * backends::trace_gemm_op(1, KC, S)
+      + backends::trace_gemm_op(1, CtKC, SSt);
    S_old = Backend::copy_matrix(S);
 
+   f_S_mem.pop_front();
+   f_S_mem.push_back(current_cost);
+
+   double f_max = -std::numeric_limits<double>::max();
+   for (std::size_t i = 0; i < mem; ++i) {
+      if (f_S_mem[i] > f_max) {
+         f_max = f_S_mem[i];
+      }
+   }
+
    const double incr_norm = backends::sum_hadamard_op(1, incr_S, incr_S);
+   double lambda = 1;
+
+   backends::geam(1, S_old, 1, incr_S, S);
+
+   backends::gemm(1, S, S, 0, SSt, backends::Op_flag::None,
+                  backends::Op_flag::Transpose);
+
+   double next_cost = trace_K - 2 * backends::trace_gemm_op(1, KC, S)
+      + backends::trace_gemm_op(1, CtKC, SSt);
 
    int error = 0;
-   bool finished_searching = false;
-   while (!finished_searching) {
-      backends::geam(1, S_old, -alpha_S, incr_S, S);
-      backends::threshold_min(S, 0);
-      backends::normalize_columns_by_lpnorm(S, 1, safety);
+   const double factor = line_search_parameters.gamma * incr_norm;
+   while (next_cost > f_max + lambda * factor) {
+      lambda = line_search_parameters.get_next_step_length(
+         lambda, incr_norm, current_cost, next_cost);
+
+      backends::geam(1, S_old, lambda, incr_S, S);
 
       backends::gemm(1, S, S, 0, SSt, backends::Op_flag::None,
                      backends::Op_flag::Transpose);
 
-      const double next_cost = trace_K + backends::trace_gemm_op(-2, KC, S)
-         + backends::trace_gemm_op(1, SSt, CtKC);
-      if (next_cost <= current_cost - sigma * alpha_S * incr_norm) {
-         alpha_S /= beta;
-         finished_searching = true;
-      } else {
-         alpha_S *= beta;
-      }
+      next_cost = trace_K - 2 * backends::trace_gemm_op(1, KC, S)
+         + backends::trace_gemm_op(1, CtKC, SSt);
 
-      if (is_zero(alpha_S)) {
-         finished_searching = true;
+      if (is_zero(lambda)) {
+         break;
       }
    }
 
-   return error;
+   return std::make_tuple(error, lambda);
 }
 
 template <class Backend>
 int KernelAA<Backend>::update_weights()
 {
-   const std::size_t n_components = backends::rows(S);
-   const std::size_t n_samples = backends::cols(S);
+   backends::gemm(1, S, S, 0, SSt, backends::Op_flag::None,
+                  backends::Op_flag::Transpose);
 
-   backends::gemm(1, K, C, 1, KC);
+   backends::gemm(1, K, C, 0, KC);
    if (delta > 0) {
       backends::gemm(1, KC, diag_alpha, 0, KC);
    }
 
-   backends::gemm(1, C, KC, 0, CtKC,
+   backends::gemm(1, KC, C, 0, CtKC,
                   backends::Op_flag::Transpose);
    if (delta > 0) {
-      backends::gemm(1, diag_alpha, CtKC, 0, CtKC);
+      backends::gemm(1, CtKC, diag_alpha, 0, CtKC);
    }
 
    // update gradient of cost function with respect to weights
    update_weights_gradient();
 
    // compute next increment
-   Matrix projector = Backend::create_constant_matrix(n_components, 1, 1);
-   Matrix sums = Backend::create_matrix(1, n_samples);
-   backends::gemm(1, grad_S, S, 0, incr_S,
-                  backends::Op_flag::Transpose);
-   for (std::size_t i = 0; i < n_samples; ++i) {
-      backends::set_matrix_element(
-         0, i, backends::get_matrix_element(i, i, incr_S), sums);
-   }
-   backends::gemm(1, projector, sums, 0, incr_S);
-   backends::geam(1, grad_S, -1, incr_S, incr_S);
+   backends::geam(1, S, -alpha_S, grad_S, incr_S);
+   backends::simplex_project_columns(incr_S);
+   backends::geam(1, incr_S, -1, S, incr_S);
 
    // update weights by line search
-   const int error = weights_line_search();
+   const auto line_search_result = weights_line_search();
+   int error = std::get<0>(line_search_result);
+   const double lambda = std::get<1>(line_search_result);
+
+   delta_grad_S = Backend::copy_matrix(grad_S);
+
+   update_weights_gradient();
+
+   backends::geam(1, grad_S, -1, delta_grad_S, delta_grad_S);
+
+   const double sksk = backends::sum_hadamard_op(
+      lambda * lambda, incr_S, incr_S);
+   const double beta = backends::sum_hadamard_op(
+      lambda, incr_S, delta_grad_S);
+
+   alpha_S = line_search_parameters.get_next_alpha(beta, sksk);
 
    return error;
 }
@@ -264,66 +298,82 @@ int KernelAA<Backend>::update_weights()
 template <class Backend>
 void KernelAA<Backend>::update_dictionary_gradient()
 {
-   const double normalization = 1.0 / trace_K;
-
-   backends::gemm(1, K, C, 1, KC);
-   if (delta > 0) {
-      backends::gemm(1, KC, diag_alpha, 0, KC);
-   }
-
    backends::gemm(1, K, S, 0, grad_C,
-                  backends::Op_flag::None, backends::Op_flag::Transpose);
-   backends::gemm(normalization, KC, SSt, -normalization, grad_C);
+                  backends::Op_flag::Transpose,
+                  backends::Op_flag::Transpose);
 
+   backends::gemm(2, KC, SSt, -2, grad_C);
    if (delta > 0) {
       backends::gemm(1, grad_C, diag_alpha, 0, grad_C);
    }
 }
 
 template <class Backend>
-int KernelAA<Backend>::dictionary_line_search()
+std::tuple<int, double> KernelAA<Backend>::dictionary_line_search()
 {
-   const double safety = std::numeric_limits<double>::min();
+   const double current_cost = trace_K
+      - 2 * backends::trace_gemm_op(1, KC, S)
+      + backends::trace_gemm_op(1, SSt, CtKC);
 
-   const double current_cost = cost();
    C_old = Backend::copy_matrix(C);
 
+   f_C_mem.pop_front();
+   f_C_mem.push_back(current_cost);
+
+   double f_max = -std::numeric_limits<double>::max();
+   for (std::size_t i = 0; i < mem; ++i) {
+      if (f_C_mem[i] > f_max) {
+         f_max = f_C_mem[i];
+      }
+   }
+
    const double incr_norm = backends::sum_hadamard_op(1, incr_C, incr_C);
+   double lambda = 1;
+
+   backends::geam(1, C_old, 1, incr_C, C);
+
+   backends::gemm(1, K, C, 0, KC);
+   if (delta > 0) {
+      backends::gemm(1, KC, diag_alpha, 0, KC);
+   }
+   backends::gemm(1, C, KC, 0, CtKC,
+                  backends::Op_flag::Transpose);
+   if (delta > 0) {
+      backends::gemm(1, diag_alpha, CtKC, 0, CtKC);
+   }
+
+   double next_cost = trace_K
+      - 2 * backends::trace_gemm_op(1, KC, S)
+      + backends::trace_gemm_op(1, SSt, CtKC);
 
    int error = 0;
-   bool finished_searching = false;
-   while (!finished_searching) {
-      backends::geam(1, C_old, -alpha_C, incr_C, C);
-      backends::threshold_min(C, 0);
-      backends::normalize_columns_by_lpnorm(C, 1, safety);
+   const double factor = line_search_parameters.gamma * incr_norm;
+   while (next_cost > f_max + factor * lambda) {
+      lambda = line_search_parameters.get_next_step_length(
+         lambda, incr_norm, current_cost, next_cost);
 
-      backends::gemm(1, K, C, 1, KC);
+      backends::geam(1, C_old, lambda, incr_C, C);
+
+      backends::gemm(1, K, C, 0, KC);
       if (delta > 0) {
          backends::gemm(1, KC, diag_alpha, 0, KC);
       }
-
       backends::gemm(1, C, KC, 0, CtKC,
                      backends::Op_flag::Transpose);
       if (delta > 0) {
          backends::gemm(1, diag_alpha, CtKC, 0, CtKC);
       }
 
-      const double next_cost = trace_K + backends::trace_gemm_op(-2, KC, S)
+      next_cost = trace_K
+         - 2 * backends::trace_gemm_op(1, KC, S)
          + backends::trace_gemm_op(1, SSt, CtKC);
 
-      if (next_cost <= current_cost - sigma * alpha_C * incr_norm) {
-         alpha_C /= beta;
-         finished_searching = true;
-      } else {
-         alpha_C *= beta;
-      }
-
-      if (is_zero(alpha_C)) {
-         finished_searching = true;
+      if (is_zero(lambda)) {
+         break;
       }
    }
 
-   return error;
+   return std::make_tuple(error, lambda);
 }
 
 template <class Backend>
@@ -388,46 +438,45 @@ int KernelAA<Backend>::scale_factors_line_search()
 template <class Backend>
 int KernelAA<Backend>::update_dictionary()
 {
-   const std::size_t n_samples = backends::rows(C);
-   const std::size_t n_components = backends::cols(C);
-
-   // enforce initial normalization
-   Matrix inv_alpha = Backend::create_diagonal_matrix(n_components, 1);
-   if (delta > 0) {
-      for (std::size_t i = 0; i < n_components; ++i) {
-         double sum = 0;
-         for (std::size_t t = 0; t < n_samples; ++t) {
-            sum += backends::get_matrix_element(t, i, C);
-         }
-         backends::set_matrix_element(i, i, sum, diag_alpha);
-         backends::set_matrix_element(i, i, 1. / sum, inv_alpha);
-      }
-
-      backends::gemm(1, C, inv_alpha, 0, C);
-   }
-
-   backends::gemm(1, S, S, 1, SSt,
+   backends::gemm(1, S, S, 0, SSt,
                   backends::Op_flag::None, backends::Op_flag::Transpose);
+
+   backends::gemm(1, K, C, 0, KC);
+   if (delta > 0) {
+      backends::gemm(1, KC, diag_alpha, 0, KC);
+   }
+   backends::gemm(1, C, KC, 0, CtKC,
+                  backends::Op_flag::Transpose);
+   if (delta > 0) {
+      backends::gemm(1, diag_alpha, CtKC, 0, CtKC);
+   }
 
    // update gradient of cost function with respect to dictionary
    update_dictionary_gradient();
 
    // compute next increment
-   Matrix projector = Backend::create_constant_matrix(n_samples, 1, 1);
-   Matrix sums = Backend::create_matrix(1, n_components);
-   Matrix gC = Backend::create_matrix(n_components, n_components);
-   backends::gemm(1, grad_C, C, 0, gC,
-                  backends::Op_flag::Transpose);
-
-   for (std::size_t i = 0; i < n_components; ++i) {
-      backends::set_matrix_element(
-         0, i, backends::get_matrix_element(i, i, gC), sums);
-   }
-   backends::gemm(1, projector, sums, 0, incr_C);
-   backends::geam(1, grad_C, -1, incr_C, incr_C);
+   backends::geam(1, C, -alpha_C, grad_C, incr_C);
+   backends::simplex_project_columns(incr_C);
+   backends::geam(1, incr_C, -1, C, incr_C);
 
    // update dictionary element by line search
-   int error = dictionary_line_search();
+   const auto line_search_result = dictionary_line_search();
+   int error = std::get<0>(line_search_result);
+   const double lambda = std::get<1>(line_search_result);
+
+   // update alpha for next iteration
+   delta_grad_C = Backend::copy_matrix(grad_C);
+
+   update_dictionary_gradient();
+
+   backends::geam(1, grad_C, -1, delta_grad_C, delta_grad_C);
+
+   const double sksk = backends::sum_hadamard_op(
+      lambda * lambda, incr_C, incr_C);
+   const double beta = backends::sum_hadamard_op(
+      lambda, incr_C, delta_grad_C);
+
+   alpha_C = line_search_parameters.get_next_alpha(beta, sksk);
 
    if (delta > 0) {
       // update gradient of cost function with respect to scale factors
