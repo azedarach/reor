72a316a4ea2bd030fad76d7670858dbb4ea54bba
Dylan Harries <dtc.harries@gmail.com>
2019-10-10 10:39:58 +1100

    Remove unused CMake files and third-party libraries

--- cmake/ReorBuildOptions.cmake
+++
@@ -1,3 +0,0 @@
-option(ENABLE_COMPILER_WARNINGS "Enable compiler warnings" ON)
-option(ENABLE_PYTHON_BINDINGS "Enable building Python bindings" ON)
-option(ENABLE_TESTING "Enable building unit tests" ON)
