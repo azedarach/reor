853d2206364ba9125b38af0d5c9b42c71c2b0f83
Dylan Harries <dtc.harries@gmail.com>
2019-10-22 13:36:36 +1100

    Add separate routines for fitting VAR models
    for subsequent use in the FEM-BV models and to permit re-use and 
    comparison to simpler models.

---
+++ tests/test_var.py
@@ -0,0 +1,61 @@
+"""
+Provides test routines for VAR routines.
+"""
+
+import unittest
+import os
+import numpy as np
+
+from reor.var import linear_varx_leastsq
+
+
+TEST_DATA_PATH = os.path.realpath(os.path.dirname(__file__))
+
+
+class TestLinearLeastSquaresVAR(unittest.TestCase):
+    """Provides unit tests for linear VAR models with least squares."""
+
+    def test_linear_varx_leastsq_example_1(self):
+        """Test gives correct least-squares estimate for example dataset."""
+
+        p = 2
+
+        data = np.genfromtxt(os.path.join(TEST_DATA_PATH, 'test_dataset_1.csv'),
+                             delimiter=',', names=True)
+
+        # Use data from 1960Q1 to 1978Q4
+        mask = data['year'] <= 1978
+
+        invest = data['invest'][mask]
+        income = data['income'][mask]
+        cons = data['cons'][mask]
+
+        # Fits are performed on first differences of log-data
+        log_invest = np.log(invest)
+        log_income = np.log(income)
+        log_cons = np.log(cons)
+
+        y = np.vstack(
+            [np.diff(log_invest), np.diff(log_income), np.diff(log_cons)])
+
+        self.assertTrue(y.shape == (3, 75))
+
+        mu, A, B, B0, Sigma_LS = linear_varx_leastsq(
+            y, p=p, include_intercept=True, bias=False)
+
+        expected_mu = np.array([-0.017, 0.016, 0.013])
+        expected_A = np.array([[[-0.320, 0.146, 0.961],
+                                [0.044, -0.153, 0.289],
+                                [-0.002, 0.225, -0.264]],
+                               [[-0.161, 0.115, 0.934],
+                                [0.050, 0.019, -0.010],
+                                [0.034, 0.355, -0.022]]])
+        expected_Sigma_LS = np.array([[21.30e-4, 0.72e-4, 1.23e-4],
+                                      [0.72e-4, 1.37e-4, 0.61e-4],
+                                      [1.23e-4, 0.61e-4, 0.89e-4]])
+
+        self.assertTrue(np.allclose(mu, expected_mu, atol=1e-3))
+        self.assertTrue(np.allclose(A, expected_A, atol=1e-3))
+        self.assertTrue(B is None)
+        self.assertTrue(B0 is None)
+        self.assertTrue(np.allclose(Sigma_LS, expected_Sigma_LS, atol=1e-4))
