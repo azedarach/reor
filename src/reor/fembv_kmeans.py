59c3c7440d7f29a21b085b39ff15090c0498ca7f
Dylan Harries <dtc.harries@gmail.com>
2019-10-23 17:24:17 +1100

    Add documentation for FEM-BV-k-means

--- src/reor/fembv_kmeans.py
+++ src/reor/fembv_kmeans.py
@@ -56,7 +56,71 @@ def _initialize_fembv_kmeans_random(X, n_components, random_state=None):
 
 
 class FEMBVKMeans(FEMBV):
-    """FEM-BV-k-means clustering of data."""
+    """FEM-BV-k-means clustering of data.
+
+    Parameters
+    ----------
+    n_components : integer or None
+        If an integer, the number of components. If None, then all features
+        are kept.
+
+    max_tv_norm : None or float, default: None
+        If a number, the maximum total-variation norm allowed for the
+        weights sequence. If None, no TV constraint is imposed.
+
+    init : None | 'random' | 'custom'
+        Method used to initialize the algorithm.
+        Default: None
+        Valid options:
+
+        - None: falls back to 'random'.
+
+        - 'random': weights and parameters are initialized to random values.
+
+        - 'custom': use custom matrices for weights and parameters. If given,
+          the values of the parameters must be provided as a dict with keys
+          'mu', 'A', and 'B0', for which the corresponding values should be the
+          initial guess for each parameter.
+
+    max_iterations : integer, default: 500
+        Maximum number of iterations before stopping.
+
+    tolerance : float, default: 1e-4
+        Tolerance of the stopping condition.
+
+    verbose : integer, default: 0
+        The verbosity level.
+
+    random_state : integer, RandomState, or None
+        If an integer, random_state is the seed used by the
+        random number generator. If a RandomState instance,
+        random_state is the random number generator. If None,
+        the random number generator is the RandomState instance
+        used by `np.random`.
+
+    Attributes
+    ----------
+    Gamma : array-like, shape (n_samples, n_components)
+        The fitted affiliations for the model.
+
+    Theta : array-like, shape (n_components, n_features)
+        The cluster centroids.
+
+    Examples
+    --------
+    import numpy as np
+    X = np.random.rand(10, 4)
+    from reor.fembv_kmeans import FEMBVKMeans
+    model = FEMBVKMeans(n_components=2, init='random', random_state=0)
+    gamma = model.fit_transform(X)
+    Theta = model.Theta
+
+    References
+    ----------
+    P. Metzner, L. Putzig, and I. Horenko, "Analysis of Persistent and
+    Nonstationary Time Series and Applications", Commun. Appl. Math. Comput.
+    Sci. 7, 2 (2012), 175 - 229
+    """
 
     def __init__(self, n_components, max_tv_norm, init='random',
                  max_iterations=500, tolerance=1e-4, verbose=0,
